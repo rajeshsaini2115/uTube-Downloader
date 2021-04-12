9cbd7f84f7990edbf13964b42bc13731cb75d7ff
TG Expert <78008481+v-v-r-official@users.noreply.github.com>
2021-04-12 08:38:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -19,7 +19,7 @@ async def start(bot, update):
 
 
 @Client.on_message(filters.command(["help"]) & filters.private)
-async def start(bot, update):
+async def help(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.HELP_TEXT.format(update.from_user.mention),
