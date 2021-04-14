3cf6f0e2298c4c5b30d0d5751016669df1eb3425
TG Expert <78008481+v-v-r-official@users.noreply.github.com>
2021-04-14 09:35:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -27,3 +27,13 @@ async def help(bot, update):
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ JOIN UPDATES CHANNEL ⚙', url='https://telegram.me/VKPROJECTS')]]),
         reply_to_message_id=update.message_id
     )
+
+@Client.on_message(filters.command(["about"]) & filters.private)
+async def about(bot, update):
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.ABOUT_TEXT.format(update.from_user.mention),
+        parse_mode="html", disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton('⚙ JOIN UPDATES CHANNEL ⚙', url='https://telegram.me/VKPROJECTS')]]),
+        reply_to_message_id=update.message_id
+    )
